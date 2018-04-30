f45e047676ee0aafa8a3665df1dba603c8a8ee80
maolin <772984968@qq.com>
2018-04-30 17:37:37 +0800

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,24 +5,14 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@include('layouts._header');
-<header class="navbar navbar-fixed-top navbar-inverse">
-    <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-            <a href="/" id="logo">Sample App</a>
-            <nav>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="/help">帮助</a></li>
-                    <li><a href="#">登录</a></li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
+@include('layouts._header')
 
 <div class="container">
-    @yield('content')
-    @include('layouts._footer')
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
 </body>
 </html>
